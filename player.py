11a36a3c6e67637acd8b23210a6bde93d1585a1f
AbdelRahman Soliman <abdelrahmaninfra@proton.me>
2026-05-05 12:37:59 +0300

    Attack Function added to player.py

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 from entity import Entity
-
+import random
 
 class Player(Entity):
     def __init__(self, name: str = None, hp: int = 100, level: int = 1, attack_power: int = 1):
@@ -9,3 +9,8 @@ class Player(Entity):
     def heal(self, amount):
         self.hp += amount
         print(f"{self.name} healed for {amount} hp")
+        
+    def attack(self, player):
+        damage = random.randint(5,20) * self.level
+        player.take_damage(damage)
+        print(f"{self.name} attacked {player.name} for {damage} damage")
