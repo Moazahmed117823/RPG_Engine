fb50f50d2f95b78ecac234ce90baeb5a1fb6a212
AbdelRahman Soliman <abdelrahmaninfra@proton.me>
2026-05-06 21:52:33 +0300

    Add new combatmanager to main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from player import Player
 from enemy import Enemy 
-from combatmanager import CombatManager
+from combat_manager import CombatManager
 
 p = Player("Player001", 100, 10, 10)
 e = Enemy("Villain001", 100, 5, 10)
