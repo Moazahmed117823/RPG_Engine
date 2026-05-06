8f6dec327c6dd4abd3bb9a984a5610bf5bb72fce
AbdelRahman Soliman <abdelrahmaninfra@proton.me>
2026-05-06 14:29:58 +0300

    add main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from player import Player
 from enemy import Enemy 
-from combat import CombatManager
+from combatmanager import CombatManager
 
 p = Player("Player001", 100, 10, 10)
 e = Enemy("Villain001", 100, 5, 10)
 
 combat = CombatManager(p, e)
-combat.start()
+combat.Start()
+1
